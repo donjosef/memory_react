93a8dc0cee9f445bb486fc9d306a1ff4ceebab81
donjoe <giumontanaro@gmail.com>
2018-10-10 12:11:51 +0200

    Create a Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+
+const card = props => {
+
+    return (
+        <li
+            className='Card'>
+        </li>
+    )
+}
+
+export default card;
