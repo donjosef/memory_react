afd759cd1e51f647bdfe0285a913c89089d4b494
donjoe <giumontanaro@gmail.com>
2018-10-10 19:00:49 +0200

    Add test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import GameOver from './components/GameOver/GameOver';
+
+configure({
+    adapter: new Adapter()
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe(' <App />', () => {
+    it('should render <GameOver /> if every card is open', () => {
+      const wrapper = shallow(<App />);
+      wrapper.setState({cards: [ {opened: true} ]});
+      expect(wrapper.find(GameOver)).toHaveLength(1);        
+    });
 });
