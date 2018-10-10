cad8d9f4295facf18783a3bb765dd508152742f4
donjoe <giumontanaro@gmail.com>
2018-10-10 13:28:34 +0200

    Apply Success class to cards with a match

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Card.css';
 
 const card = props => {
-    const {color, opened, show, id} = props;
+    const {color, opened, show, id, match} = props;
+    let classes = ['Card'];
+    if(match === true) {
+        classes.push('Success');
+    }
     return (
         <li
           onClick={() => show(id)}
-          className='Card'
+          className={classes.join(' ')}
           style={{background: opened ? color : '#f9f9f9'}}>
         </li>
     )
